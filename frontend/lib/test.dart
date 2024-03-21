d644cf31cb108a87ad5bd55824d195359a04dff1
Gongchaeyeon <dnjftjd53@kookmin.ac.kr>
2024-03-21 23:18:38 +0900

    [fix] main화면으로 테스트 -> test.dart에서 연결 하도록 수정

--- frontend/lib/test.dart
+++ frontend/lib/test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/test/darthello.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const dartHello(); // 여기에 연결되는 스크린
   }
 }
