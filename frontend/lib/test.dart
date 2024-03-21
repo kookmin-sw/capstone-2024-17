eb4aa52b9dc5cce56de0b876ee7cc43532d3cacc
Gongchaeyeon <dnjftjd53@kookmin.ac.kr>
2024-03-22 02:14:12 +0900

    [feat] Google Maps api 연동 & 지도 띄우기

--- frontend/lib/test.dart
+++ frontend/lib/test.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/test/darthello.dart';
+import 'package:frontend/screen/map.dart';
+
+
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const dartHello(); // 여기에 연결되는 스크린
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: mapScreen(),
+    );
   }
 }
