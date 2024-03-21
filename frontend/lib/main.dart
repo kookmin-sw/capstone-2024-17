6063306b84923e7889bc553930099e95899fc4e4
jm3789 <jm37890@gmail.com>
2024-03-21 23:02:23 +0900

    [chore] #8 카카오 SDK 추가

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_common.dart';
 
 void main() async {
   await dotenv.load();
+  KakaoSdk.init(nativeAppKey: dotenv.env['KAKAO_NATIVE_APP_KEY']);
   runApp(const MyApp());
 }
 
