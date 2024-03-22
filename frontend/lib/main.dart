9ccb731f838e838668c2bee311e4d094a65c8a69
jm3789 <jm37890@gmail.com>
2024-03-22 23:38:45 +0900

    [feat] #8 카카오톡 로그인 구현

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_common.dart';
+import 'package:frontend/login.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
 void main() async {
   await dotenv.load();
