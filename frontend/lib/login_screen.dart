e1c38db07ce83abc973d4b69460fcd56a3519a92
jm3789 <jm37890@gmail.com>
2024-03-25 02:30:59 +0900

    [style] #8 로그인 페이지 디자인 변경

--- frontend/lib/login_screen.dart
+++ frontend/lib/login_screen.dart
@@ -25,20 +25,38 @@ class _loginScreenState extends State<LoginScreen> {
           child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Text('${viewModel.isLogined}'),
-          ElevatedButton(
-            onPressed: () async {
-              await viewModel.login();
-              setState(() {}); // 화면 갱신
-            },
-            child: const Text('Login'),
+          Container(
+              padding: const EdgeInsets.all(20),
+              child: const Text('로그인',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ))),
+          Text('로그인 상태: ${viewModel.isLogined}'), // 로그인되었는지 상태 출력
+          Image.network(
+            viewModel.user?.kakaoAccount?.profile?.profileImageUrl ?? '',
+            width: 50,
+            height: 50,
           ),
-          ElevatedButton(
+          Text(
+            '이름: ${viewModel.user?.kakaoAccount?.profile?.nickname ?? ' - '}',
+          ),
+          Container(
+            margin: const EdgeInsets.all(10),
+            child: InkWell(
+              onTap: () async {
+                await viewModel.login();
+                setState(() {}); // 화면 갱신
+              },
+              child: Image.asset('assets/kakao_login_medium_narrow.png'),
+            ),
+          ),
+          TextButton(
             onPressed: () async {
               await viewModel.logout();
               setState(() {}); // 화면 갱신
             },
-            child: const Text('Logout'),
+            child: const Text('로그아웃'),
           ),
         ],
       )),
