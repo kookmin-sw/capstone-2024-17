d71156a4dcfddde9547c3aa55fab46706fb97ab8
jm3789 <jm37890@gmail.com>
2024-03-25 02:47:06 +0900

    [chore] #8 자바스크립트 키 추가

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -5,7 +5,11 @@ import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
 void main() async {
   await dotenv.load();
-  KakaoSdk.init(nativeAppKey: dotenv.env['KAKAO_NATIVE_APP_KEY']);
+  KakaoSdk.init(
+    nativeAppKey: dotenv.env['KAKAO_NATIVE_APP_KEY'],
+    // 웹으로 실행하는 경우 필요한 키
+    javaScriptAppKey: dotenv.env['JAVA_SCRIPT_APP_KEY'],
+  );
   runApp(const MyApp());
 }
 
