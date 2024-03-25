682f9b864b679d0f64f9da76922c0ddac5f9814d
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2024-03-25 21:22:53 +0900

    [add] Backend 개발환경 세팅

---
+++ backend/src/main/java/com/coffee/backend/BackendApplication.java
@@ -0,0 +1,22 @@
+package com.coffee.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class BackendApplication {
+
+    @GetMapping(value = "")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
