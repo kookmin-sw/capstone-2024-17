e18dd920cecf854b65a927ce01d3400cacb720e7
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2024-03-25 21:39:26 +0900

    [init] 에러 핸들링 및 Response 형태 통일

---
+++ backend/src/main/java/com/coffee/backend/exception/CustomException.java
@@ -0,0 +1,25 @@
+package com.coffee.backend.exception;
+
+public class CustomException extends RuntimeException {
+
+    private final ErrorCode code;
+
+    public CustomException(ErrorCode code, String message) {
+        super(code.getMessage() + "(" + message + ")");
+        this.code = code;
+    }
+
+    public CustomException(ErrorCode code) {
+        super(code.getMessage());
+        this.code = code;
+    }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+
+    public ErrorCode getCode() {
+        return this.code;
+    }
+}
