b165b7f686e47a4e6f04a790e26b35d897ae9d2b
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2024-03-26 14:30:18 +0900

    [refactor] 성공 응답 생성 Builder 객체로 수정

--- backend/src/main/java/com/coffee/backend/utils/ApiResponse.java
+++ backend/src/main/java/com/coffee/backend/utils/ApiResponse.java
@@ -1,31 +1,61 @@
 package com.coffee.backend.utils;
 
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
 @Getter
-@Setter
 public class ApiResponse<T> {
-    private boolean success;
-    private String code;
-    private String message;
-    private T data;
+    private final boolean success;
+    private final String statusCode;
+    private final String message;
+    private final T data;
 
-    public ApiResponse(boolean success, String code, String message, T data) {
-        this.success = success;
-        this.code = code;
-        this.message = message;
-        this.data = data;
+    public static class Builder<T> {
+        private boolean success;
+        private String statusCode;
+        private String message;
+        private T data;
+
+        public Builder<T> success(boolean success) {
+            this.success = success;
+            return this;
+        }
+
+        public Builder<T> statusCode(String statusCode) {
+            this.statusCode = statusCode;
+            return this;
+        }
+
+        public Builder<T> message(String message) {
+            this.message = message;
+            return this;
+        }
+
+        public Builder<T> data(T data) {
+            this.data = data;
+            return this;
+        }
+
+        public ApiResponse<T> build() {
+            return new ApiResponse<>(this);
+        }
     }
 
-    public boolean isSuccess() {
-        return success;
+    private ApiResponse(Builder<T> builder) {
+        this.success = builder.success;
+        this.statusCode = builder.statusCode;
+        this.message = builder.message;
+        this.data = builder.data;
     }
 
     // 성공 응답 생성
     public static <T> ApiResponse<T> success(T data) {
-        return new ApiResponse<>(true, String.valueOf(HttpStatus.OK.value()), "Success", data);
+        return new Builder<T>()
+                .success(true)
+                .statusCode(String.valueOf(HttpStatus.OK.value()))
+                .message("Success")
+                .data(data)
+                .build();
     }
 
 }
