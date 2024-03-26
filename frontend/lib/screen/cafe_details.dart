04c41881fdd73d5a848dfced3ce6a6952152059a
godeka <godeka@naver.com>
2024-03-27 01:10:06 +0900

    [refactor]상단 Row위젯 AppBar로 수정 #6

--- frontend/lib/screen/cafe_details.dart
+++ frontend/lib/screen/cafe_details.dart
@@ -9,49 +9,39 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Padding(
+        appBar: AppBar(
+          title: const Text(
+            "스타벅스 국민대점",
+            style: TextStyle(fontSize: 24),
+          ),
+          toolbarHeight: 100,
+          backgroundColor: Colors.white,
+          elevation: 1.0,
+          shadowColor: Colors.black,
+          leading: const Icon(Icons.arrow_back_ios),
+          leadingWidth: 70,
+        ),
+        body: const Padding(
           padding: EdgeInsets.symmetric(vertical: 50, horizontal: 20),
           child: Column(
             children: [
-              Row(
-                children: [
-                  Icon(
-                    Icons.arrow_back,
-                    size: 30,
-                  ),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Text(
-                    "스타벅스 국민대점",
-                    style: TextStyle(fontSize: 25),
-                  ),
-                ],
+              UserItem(
+                nickname: "뽕순이",
+                company: "채연컴퍼니",
+                position: "집사",
+                introduction: "안녕하세요 뽕순이입니다 뽕",
               ),
-              SizedBox(
-                height: 30,
-              ),
-              Column(
-                children: [
-                  UserItem(
-                    nickname: "뽕순이",
-                    company: "채연컴퍼니",
-                    position: "집사",
-                    introduction: "안녕하세요 뽕순이입니다 뽕",
-                  ),
-                  UserItem(
-                      nickname: "담",
-                      company: "네카라쿠배당토",
-                      position: "웹 프론트엔드",
-                      introduction: "안녕하세욯ㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎ"),
-                ],
-              ),
+              UserItem(
+                  nickname: "담",
+                  company: "네카라쿠배당토",
+                  position: "웹 프론트엔드",
+                  introduction: "안녕하세욯ㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎ"),
             ],
           ),
         ),
-        bottomNavigationBar: BottomAppBar(),
+        bottomNavigationBar: const BottomAppBar(),
       ),
     );
   }
