58bfe3aa7ec36c5f9a45d4559f0bf42c3622c8d5
Gongchaeyeon <dnjftjd53@kookmin.ac.kr>
2024-03-29 00:38:21 +0900

    [feat] 현재 위치로 이동하는 기능 추가 & 마커 표시 방법 변경

--- frontend/ios/Runner/AppDelegate.swift
+++ frontend/ios/Runner/AppDelegate.swift
@@ -8,7 +8,8 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("@string/GOOGLE_MAP_KEY")
+   //아래의 key: ios api key로 추후 변경 & 보안 필요
+    GMSServices.provideAPIKey("AIzaSyAtd0czAN7ZeaQprSP5ASC-2R9yazKBBiU")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- frontend/lib/screen/map.dart
+++ frontend/lib/screen/map.dart
@@ -1,90 +1,202 @@
-import 'dart:async';
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: mapScreen(),
-  ),
-  );
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final status = await Geolocator.checkPermission();
+  if (status == LocationPermission.denied) {
+    await Geolocator.requestPermission();
+  }
+  runApp(MyApp());
 }
-class mapScreen extends StatefulWidget {
+
+class MyApp extends StatelessWidget {
   @override
-  _mapScreenState createState() => _mapScreenState();
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      home: map_widget(),
+    );
+  }
 }
 
-class _mapScreenState extends State<mapScreen> {
-  Completer<GoogleMapController> _controller = Completer();
+class map_widget extends StatefulWidget {
+  const map_widget({Key? key, required}) : super(key: key);
+
+  @override
+  State<map_widget> createState() => _map_widgetState();
+}
+
+class _map_widgetState extends State<map_widget> {
   @override
   void initState() {
-    _markers.addAll(
-      _foodies.map(
-            (e) => Marker(
-          markerId: MarkerId(e['name'] as String),
-          infoWindow: InfoWindow(title: e['name'] as String),
-          position: LatLng(
-            e['latitude'] as double,
-            e['longitude'] as double,
-          ),
-        ),
-      ),
-    );
     super.initState();
+    requestLocationPermission();
   }
 
-  final _markers = <Marker>{};
-  final _foodies = [
-    {
-      "name": "카페아일랜드",
-      "latitude": 37.5925414,
-      "longitude": 127.0170871,
-    },
-    {
-      "name": "카페핸드",
-      "latitude":  37.593260320178935,
-      "longitude": 127.01515591535698,
-    },
-    {
-      "name": "펠어커피",
-      "latitude":  37.59324683702535,
-      "longitude": 127.01490680535643,
-    },
-    {
-      "name": "멜랑슈 에스프레소",
-      "latitude": 37.593311913011235,
-      "longitude": 127.01674115525596,
-    },
-    {
-      "name": "투썸플레이스",
-      "latitude": 37.59354169227075,
-      "longitude": 127.01655437554344,
-    },
-  ];
+  late GoogleMapController _controller;
+  bool _myLocationEnabled = false;
 
-  CameraPosition _currentPosition = CameraPosition(
-    // target: LatLng(37.611108, 126.997340), //사용자 지정 좌표 (학교)
-    target: LatLng(37.5925683,127.0164784), //성신여대 입구
-    zoom: 18, //확대
-  );
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
+  Future<void> requestLocationPermission() async {
+    PermissionStatus status = await Permission.location.request();
+    if (status.isDenied) {
+      print('사용자가 위치 권한을 거부했습니다.');
+    } else if (status.isGranted) {
+      print('사용자가 위치 권한을 허용했습니다.');
+    }
+  }
+
+  Future<void> _getCurrentLocation() async {
+    final position = await Geolocator.getCurrentPosition();
+    final cameraPosition = CameraPosition(
+      target: LatLng(position.latitude, position.longitude),
+      zoom: 18,
+    );
+    _controller.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+    setState(() {
+      _myLocationEnabled = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Map<String, dynamic>> places = [
+      {
+        "name": "카페아일랜드",
+        "latitude": 37.5925414,
+        "longitude": 127.0170871,
+      },
+      {
+        "name": "카페핸드",
+        "latitude": 37.593260320178935,
+        "longitude": 127.01515591535698,
+      },
+      {
+        "name": "펠어커피",
+        "latitude": 37.59324683702535,
+        "longitude": 127.01490680535643,
+      },
+      {
+        "name": "멜랑슈 에스프레소",
+        "latitude": 37.593311913011235,
+        "longitude": 127.01674115525596,
+      },
+      {
+        "name": "투썸플레이스",
+        "latitude": 37.59354169227075,
+        "longitude": 127.01655437554344,
+      },
+    ];
+    return CupertinoPageScaffold(
+      child: Stack(
+        children: [
+          GoogleMap(
+            initialCameraPosition: CameraPosition(
+              target: LatLng(37.5925683, 127.0164784), //성신여대 입구
+              zoom: 18,
+            ),
+            myLocationEnabled: _myLocationEnabled,
+            myLocationButtonEnabled: false,
+            markers: Set<Marker>.of(places.map((place) {
+              return Marker(
+                markerId: MarkerId(place['name']),
+                position: LatLng(place['latitude'], place['longitude']),
+                infoWindow: InfoWindow(title: place['name']),
+                onTap: () { //마커 클릭 시 이동하는 곳
+                  Navigator.push( 
+                      context,
+                      CupertinoPageRoute(
+                          builder: (context) => MarkerDetailScreen()));
+                },
+              );
+            })),
+            onMapCreated: (controller) => _controller = controller,
+          ),
+          Positioned(
+            bottom: 25,
+            left: 16,
+            child: FloatingActionButton( //현재 위치 표시하는 버튼
+              onPressed: _getCurrentLocation, //현재 위치 구하는 함수로 이동
+              foregroundColor: Colors.black,
+              backgroundColor: Colors.white,
+              elevation: 8, // 그림자 크기
+              child: Icon(Icons.my_location),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10), // 현재 버튼 모서리 둥글기
+              ),
+            ),
+          ),
+          Positioned(
+            top: 54,
+            child: CupertinoButton(
+              onPressed: () {
+                Navigator.push( //상단바의 위치검색 클릭 시 이동
+                    context,
+                    CupertinoPageRoute(
+                        builder: (context) => MarkerDetailScreen()));
+              },
+              child: Container(
+                width: MediaQuery.of(context).size.width - 21,
+                height: 44,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16.0),
+                      child: Text(
+                        '위치 검색',
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                    const Spacer(),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 13.0),
+                    ),
+                  ],
+                ),
+                decoration: BoxDecoration( // 위치 검색 버튼 꾸미는 곳
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(8))),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MarkerDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Google Maps"),
+        title: Text('Marker Detail'),
       ),
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        child: GoogleMap(
-          initialCameraPosition: _currentPosition,
-          onMapCreated: (GoogleMapController controller) {
-            _controller.complete();
-          },
-          markers: _markers,
-        ),
+      body: Center(
+        child: Text('Marker detail screen'),
       ),
     );
   }
 }
+
+// class marker_widget extends StatelessWidget {
+//   const marker_widget({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
