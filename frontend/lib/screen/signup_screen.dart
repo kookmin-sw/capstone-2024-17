5ddca44645f6e3052b73f144059a7123f72c2cc0
jm3789 <jm37890@gmail.com>
2024-03-28 17:22:58 +0900

    [feat] #2 비밀번호 암호화

--- frontend/lib/screen/signup_screen.dart
+++ frontend/lib/screen/signup_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:bcrypt/bcrypt.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -179,12 +180,13 @@ class _SignupScreenState extends State<SignupScreen> {
 
   void signup(BuildContext context, String loginId, String password,
       String nickname, String email, String phone) async {
-    print(password);
+    final String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
+    // print(hashedPassword);
     final url = Uri.parse('http://localhost:8080/auth/signUp');
     // final url = Uri.parse('https://jsonplaceholder.typicode.com/todos');
     final data = jsonEncode({
       'loginId': loginId,
-      'password': password,
+      'password': hashedPassword,
       'nickname': nickname,
       'email': email,
       'phone': phone,
