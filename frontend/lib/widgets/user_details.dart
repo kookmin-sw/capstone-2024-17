6f3f3d18f821e3da7e16942faf40a65d6d18c45c
godeka <godeka@naver.com>
2024-04-01 16:52:32 +0900

    [refactor] 모달 버튼 위젯화 #7

--- frontend/lib/widgets/user_details.dart
+++ frontend/lib/widgets/user_details.dart
@@ -92,27 +92,48 @@ class UserDetails extends StatelessWidget {
                   ),
                 ),
               ),
-              SizedBox(
-                width: 280,
-                height: 50,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xff212121),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  child: const Text(
-                    "커피챗 요청하기",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                    ),
-                  ),
-                ),
+              ModalButton(
+                text: "커피챗 요청하기",
+                handlePressed: () {},
               ),
             ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ModalButton extends StatelessWidget {
+  final String text;
+  final Function handlePressed;
+
+  const ModalButton({
+    super.key,
+    required this.text,
+    required this.handlePressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 280,
+      height: 50,
+      child: ElevatedButton(
+        onPressed: () {
+          handlePressed();
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: const Color(0xff212121),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+        child: Text(
+          text,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 18,
           ),
         ),
       ),
