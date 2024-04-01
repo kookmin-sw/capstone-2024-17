c4943a202f609db786aba83f9e5e676e35bf9075
godeka <godeka@naver.com>
2024-04-01 16:34:05 +0900

    [feat] 유저 상세정보 창 #7

---
+++ frontend/lib/widgets/user_details.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class UserDetails extends StatelessWidget {
+  const UserDetails({
+    super.key,
+    required this.nickname,
+    required this.company,
+    required this.position,
+    required this.introduction,
+  });
+
+  final String nickname;
+  final String company;
+  final String position;
+  final String introduction;
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: Container(
+        width: 350,
+        height: 450,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Colors.white),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(25),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    width: 25,
+                  ),
+                  Container(
+                    width: 100,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Colors.grey),
+                    ),
+                    child: const CircleAvatar(
+                      radius: 50,
+                      backgroundColor: Colors.white,
+                      backgroundImage: AssetImage(
+                        "assets/coffee_bean.png",
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 30,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        nickname,
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        company,
+                        style: const TextStyle(fontSize: 18),
+                      ),
+                      Text(
+                        position,
+                        style: const TextStyle(fontSize: 18),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Expanded(
+                child: Container(
+                  width: 280,
+                  margin: const EdgeInsets.symmetric(vertical: 20),
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: SingleChildScrollView(
+                    child: Text(
+                      introduction,
+                      style: const TextStyle(fontSize: 18),
+                    ),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xff371d10),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+                child: const Text(
+                  "커피챗 요청하기",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- frontend/lib/widgets/user_item.dart
+++ frontend/lib/widgets/user_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/widgets/user_details.dart';
 
 class UserItem extends StatelessWidget {
   final String nickname;
@@ -16,41 +17,57 @@ class UserItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(bottom: 20),
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-      width: 400,
-      height: 100,
-      decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border.all(color: Colors.grey),
-          borderRadius: const BorderRadius.all(Radius.circular(10))),
-      child: Row(
-        children: [
-          const SizedBox(
-            width: 100,
-            child: Icon(
-              Icons.circle_notifications,
-              size: 70,
-            ),
-          ),
-          // CircleAvatar(child: Image.asset("assets/logo.png"),),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(nickname),
-              Text("$company / $position"),
-              SizedBox(
-                width: 200,
-                child: Text(
-                  introduction,
-                  overflow: TextOverflow.ellipsis,
-                ),
+    return InkWell(
+      onTap: () {
+        // 클릭 시 유저 상세보기 모달 띄우기
+        showDialog(
+          context: context,
+          builder: (context) {
+            return UserDetails(
+              nickname: nickname,
+              company: company,
+              position: position,
+              introduction: introduction,
+            );
+          },
+        );
+      },
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 20),
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+        width: 400,
+        height: 100,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: Colors.grey),
+            borderRadius: const BorderRadius.all(Radius.circular(10))),
+        child: Row(
+          children: [
+            const SizedBox(
+              width: 100,
+              child: Icon(
+                Icons.circle_notifications,
+                size: 70,
               ),
-            ],
-          )
-        ],
+            ),
+            // CircleAvatar(child: Image.asset("assets/logo.png"),),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(nickname),
+                Text("$company / $position"),
+                SizedBox(
+                  width: 200,
+                  child: Text(
+                    introduction,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
