b845e8e3c52b67aa2f9287d9b951e6dfe0cf8567
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2024-04-06 15:33:37 +0900

    [chore] @RequiredArgsConstrutor 추가

--- backend/src/main/java/com/coffee/backend/domain/redis/service/TokenStorageService.java
+++ backend/src/main/java/com/coffee/backend/domain/redis/service/TokenStorageService.java
@@ -1,21 +1,19 @@
 package com.coffee.backend.domain.redis.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
 
 @Service
+@RequiredArgsConstructor
 public class TokenStorageService {
 
     private static final long TOKEN_EXPIRATION_TIME = 3L; // 토큰 만료 시간 - 3시간
     private static final TimeUnit TOKEN_EXPIRATION_TIME_UNIT = TimeUnit.HOURS;
 
     private final RedisTemplate<String, String> redisTemplate;
-
-    public TokenStorageService(RedisTemplate<String, String> redisTemplate) {
-        this.redisTemplate = redisTemplate;
-    }
 
     public void storeToken(String userUUID, String token) {
         redisTemplate.opsForValue().set(userUUID, token, TOKEN_EXPIRATION_TIME, TOKEN_EXPIRATION_TIME_UNIT);
