ad6fc524e91a0bc3bb1766483298ace86cea697f
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2024-04-07 00:00:54 +0900

    [feat] Redis pub/sub 적용

--- backend/src/main/java/com/coffee/backend/global/RedisConfig.java
+++ backend/src/main/java/com/coffee/backend/global/RedisConfig.java
@@ -1,18 +1,52 @@
 package com.coffee.backend.global;
 
+import com.coffee.backend.domain.redis.service.RedisService;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @EnableCaching
 @Configuration
 public class RedisConfig {
+    // Redis에 데이터 저장/검색하는데 사용
     @Bean
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, String> template = new RedisTemplate<>();
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(String.class));
         return template;
+    }
+
+    // 특정 토픽 구독해서 메시지 수신/처리할 수 있도록 설정
+    @Bean
+    RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory,
+                                            MessageListenerAdapter cafeChoiceListenerAdapter,
+                                            MessageListenerAdapter matchRequestListenerAdapter) {
+
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.addMessageListener(cafeChoiceListenerAdapter, new PatternTopic("cafeChoice"));
+        container.addMessageListener(matchRequestListenerAdapter, new PatternTopic("matchRequest"));
+        return container;
+    }
+
+    // cafeChoice 토픽 메시지 수신/처리
+    @Bean
+    MessageListenerAdapter cafeChoiceListenerAdapter(RedisService redisService) {
+        return new MessageListenerAdapter(redisService, "handleCafeChoice");
+    }
+
+    // matchRequest 토픽 메시지 수신/처리
+    @Bean
+    MessageListenerAdapter matchRequestListenerAdapter(RedisService redisService) {
+        return new MessageListenerAdapter(redisService, "handleMatchRequest");
     }
 }
