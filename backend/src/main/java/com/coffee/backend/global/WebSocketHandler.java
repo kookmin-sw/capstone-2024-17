98108ed123feaf7eeb60afa58621ac65bdeb83df
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2024-04-07 00:03:59 +0900

    [feat] Websocket & Redis pub/sub 연결

---
+++ backend/src/main/java/com/coffee/backend/global/WebSocketHandler.java
@@ -0,0 +1,30 @@
+package com.coffee.backend.global;
+
+import com.coffee.backend.domain.redis.service.RedisService;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component // @Bean 따로 등록 없이 사용 가능
+@RequiredArgsConstructor
+public class WebSocketHandler extends TextWebSocketHandler {
+    private final RedisService redisService;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        // 메시지 Json으로 파싱
+        JsonNode jsonNode = objectMapper.readTree(message.getPayload());
+        String type = jsonNode.get("type").asText();
+
+        switch (type) {
+            case "cafeChoice" -> redisService.handleCafeChoice(jsonNode);
+            case "matchRequest" -> redisService.handleMatchRequest(jsonNode);
+            default -> System.out.println("Unknown message type");
+        }
+    }
+}
