5221e4f2247c5d2759b1e3ba438863b44222b438
haram8009 <haram8009@gmail.com>
2024-04-07 01:33:41 +0900

    [feat] stomp 채팅 메시지 보내기, get메시지리스트 api

--- backend/src/main/java/com/coffee/backend/global/WebSocketConfig.java
+++ backend/src/main/java/com/coffee/backend/global/WebSocketConfig.java
@@ -12,8 +12,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*");
-//                .withSockJS(); // TODO: sockjs 추가하기
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
 
     @Override
