e710dd36c47d7b5f0f17ec286970c329ea6431d4
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2024-04-07 02:53:03 +0900

    [refactor] 매칭 요청 및 카페 선택 정보 로직 수정

--- backend/src/main/java/com/coffee/backend/domain/redis/service/RedisService.java
+++ backend/src/main/java/com/coffee/backend/domain/redis/service/RedisService.java
@@ -1,12 +1,9 @@
 package com.coffee.backend.domain.redis.service;
 
-import com.coffee.backend.global.WebSocketSessionManager;
+import com.coffee.backend.domain.match.service.MatchService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.time.Duration;
-import java.util.Map;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
@@ -17,10 +14,11 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class RedisService implements MessageListener {
-    private final WebSocketSessionManager webSocketSessionManager;
+    private final MatchService matchService;
     private final StringRedisTemplate stringRedisTemplate;
     private final ObjectMapper objectMapper = new ObjectMapper();
 
+    // Redis에 메시지 도착할 때마다 호출
     @Override
     public void onMessage(Message message, byte[] pattern) {
         String channel = new String(message.getChannel());
@@ -41,56 +39,20 @@ public class RedisService implements MessageListener {
     public void handleCafeChoice(JsonNode jsonNode) {
         String userId = jsonNode.get("userId").asText();
         String cafeId = jsonNode.get("cafeId").asText();
-        publishCafeChoice(userId, cafeId);
+        updateCafeChoice(userId, cafeId);
     }
 
     public void handleMatchRequest(JsonNode jsonNode) {
         String fromUserId = jsonNode.get("fromUserId").asText();
         String toUserId = jsonNode.get("toUserId").asText();
-        String matchRequestId = createMatchRequestId(fromUserId, toUserId);
-        saveMatchRequest(matchRequestId, fromUserId, toUserId);
-        publishMatchRequest(fromUserId, toUserId);
+        matchService.sendAndSaveMatchRequest(fromUserId, toUserId);
     }
 
-    public void publishCafeChoice(String userId, String cafeId) {
+    public void updateCafeChoice(String userId, String cafeId) {
+        // 카페 정보 변동 시 Redis에 업데이트
+        stringRedisTemplate.opsForValue().set(userId, cafeId);
+
         String message = String.format("{\"userId\":\"%s\", \"cafeId\":\"%s\"}", userId, cafeId);
         stringRedisTemplate.convertAndSend("cafeChoice", message);
-    }
-
-    public void publishMatchRequest(String fromUserId, String toUserId) {
-        String message = String.format("{\"fromUserId\":\"%s\", \"toUserId\":\"%s\"}", fromUserId, toUserId);
-        stringRedisTemplate.convertAndSend("matchRequest", message);
-    }
-
-    // 매칭 요청 ID 생성
-    private String createMatchRequestId(String fromUserId, String toUserId) {
-        String baseId = fromUserId + "-" + toUserId;
-        String uniqueSuffix = UUID.randomUUID().toString().substring(0, 8); // 충돌 감소
-        return baseId + "-" + uniqueSuffix;
-    }
-
-    // 매칭 요청 저장
-    private void saveMatchRequest(String matchRequestId, String fromUserId, String toUserId) {
-        Map<String, String> matchRequestDetails = Map.of(
-                "matchRequestId", matchRequestId,
-                "fromUserId", fromUserId,
-                "toUserId", toUserId,
-                "status", "pending" // 초기 상태는 pending
-        );
-        stringRedisTemplate.opsForHash()
-                .putAll("matchRequest:" + matchRequestId, matchRequestDetails); // 이거 repository로 리팩토링 필요!
-        stringRedisTemplate.expire("matchRequest:" + matchRequestId, Duration.ofMinutes(10)); // 10분 후 만료
-    }
-
-    // 매칭 요청 수락 - 웹소켓 서버로 실시간으로 전송
-    public void acceptMatchRequest(String matchRequestId) {
-        stringRedisTemplate.opsForHash().put("matchRequest:" + matchRequestId, "status", "accepted");
-
-        // 매칭 요청에 해당하는 사용자 정보 조회
-        String fromUserId = stringRedisTemplate.opsForHash().get("matchRequest:" + matchRequestId, "fromUserId")
-                .toString();
-
-        // WebSocketSessionManager를 사용하여 사용자 A에게 매칭 수락 알림 전송
-        webSocketSessionManager.sendResponseMessage(fromUserId, "Your match request has been accepted.");
     }
 }
--- backend/src/main/java/com/coffee/backend/global/WebSocketHandler.java
+++ backend/src/main/java/com/coffee/backend/global/WebSocketHandler.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
@@ -13,12 +14,39 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 @Component // @Bean 따로 등록 없이 사용 가능
 @RequiredArgsConstructor
 public class WebSocketHandler extends TextWebSocketHandler {
+    private final WebSocketSessionManager webSocketSessionManager;
     private final RedisService redisService;
     private final ObjectMapper objectMapper = new ObjectMapper();
 
+    // 연결된 websocket session 등록
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) {
+        String userId = String.valueOf(session.getAttributes().get("userId"));
+        webSocketSessionManager.registerSession(userId, session);
+    }
+
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        // 메시지 Json으로 파싱
+
+        /*
+        [ message 형태 ]
+
+        1. 카페 선택 정보
+
+        {
+          "type": "cafeChoice",
+          "userId": "user123",
+          "cafeId": "cafe456"
+        }
+
+        2. 매칭 요청 정보
+        {
+          "type": "matchRequest",
+          "fromUserId": "user123",
+          "toUserId": "user456"
+        }
+        */
+
         JsonNode jsonNode = objectMapper.readTree(message.getPayload());
         String type = jsonNode.get("type").asText();
 
@@ -27,5 +55,12 @@ public class WebSocketHandler extends TextWebSocketHandler {
             case "matchRequest" -> redisService.handleMatchRequest(jsonNode);
             default -> throw new IllegalArgumentException("웹소켓 message type을 알 수 없습니다." + type);
         }
+    }
+
+    // 연결 해제 한 websocket session 삭제
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+        String userId = String.valueOf(session.getAttributes().get("userId"));
+        webSocketSessionManager.removeSession(userId);
     }
 }
