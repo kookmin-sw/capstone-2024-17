3ce708988737040537f6e3d375e2db0553adf310
haram8009 <haram8009@gmail.com>
2024-04-09 19:02:21 +0900

    [refactor] ChatroomController swagger 등록

--- backend/src/main/java/com/coffee/backend/domain/chatroom/controller/ChatroomController.java
+++ backend/src/main/java/com/coffee/backend/domain/chatroom/controller/ChatroomController.java
@@ -10,14 +10,14 @@ import com.coffee.backend.utils.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("chatroom")
 public class ChatroomController {
