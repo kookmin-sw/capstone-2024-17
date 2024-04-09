a79ca0ddf8e4e7fea961d78b3e67fd4f4476942f
jm3789 <jm37890@gmail.com>
2024-04-10 00:08:20 +0900

    [feat] #55 채팅방 item이 id, nickname, logoImage를 전달함

--- frontend/lib/widgets/chatroom_item.dart
+++ frontend/lib/widgets/chatroom_item.dart
@@ -1,86 +1,96 @@
 import 'package:flutter/material.dart';
 
 class ChatroomItem extends StatelessWidget {
+  final int id;
   final String nickname;
   final Image? logoImage;
-  final String message;
+  final String? message;
   final int count;
+  final Function(int id, String nickname, Image? logoImage) chatroomTapCallback;
 
   const ChatroomItem({
-    Key? key,
+    super.key,
+    required this.id,
     required this.nickname,
     required this.logoImage,
     required this.message,
     required this.count,
-  }) : super(key: key);
+    required this.chatroomTapCallback,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: Row(
-          children: [
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: CircleAvatar(
-                radius: 40,
-                backgroundImage: (logoImage == null)
-                    ? const AssetImage('assets/logo.png')
-                    : logoImage as ImageProvider<Object>,
+    return GestureDetector(
+      onTap: () {
+        // chatlist screen에서 chat screen으로 이동할 때 사용할 정보를 돌려주는 콜백함수
+        chatroomTapCallback(id, nickname, logoImage);
+      },
+      child: Card(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Row(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: CircleAvatar(
+                  radius: 40,
+                  backgroundImage: (logoImage == null)
+                      ? const AssetImage('assets/logo.png')
+                      : logoImage as ImageProvider<Object>,
+                ),
               ),
-            ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    nickname,
-                    overflow: TextOverflow.ellipsis,
-                    style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Row(
-                    // spaceBetween: child widget을 시작과 끝에 배치하고
-                    // 그 사이에 나머지 child widget을 배치
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Expanded(
-                        child: Text(
-                          message,
-                          overflow: TextOverflow.ellipsis,
-                          style: const TextStyle(
-                            fontSize: 13,
-                          ),
-                        ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      nickname,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
                       ),
-                      Container(
-                        margin: const EdgeInsets.all(12),
-                        height: 30,
-                        width: 30,
-                        decoration: BoxDecoration(
-                          color: Colors.red,
-                          borderRadius: BorderRadius.circular(50),
-                        ),
-                        child: Center(
+                    ),
+                    Row(
+                      // spaceBetween: child widget을 시작과 끝에 배치하고
+                      // 그 사이에 나머지 child widget을 배치
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
                           child: Text(
-                            (count < 100) ? count.toString() : '99+',
+                            message ?? '',
+                            overflow: TextOverflow.ellipsis,
                             style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 12,
-                              fontWeight: FontWeight.bold,
+                              fontSize: 13,
                             ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
+                        Container(
+                          margin: const EdgeInsets.all(12),
+                          height: 30,
+                          width: 30,
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                          child: Center(
+                            child: Text(
+                              (count < 100) ? count.toString() : '99+',
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
