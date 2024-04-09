588b2d3a85ce9a07aca225bf713a8dbe6e7c8ca1
hyeesw <hyeeun7904@kookmin.ac.kr>
2024-04-10 02:38:49 +0900

    feature : redis에 저장된 카페선택 데이터 조회 코드 (테스트 완)

--- backend/src/main/java/com/coffee/backend/domain/cafe/controller/CafeController.java
+++ backend/src/main/java/com/coffee/backend/domain/cafe/controller/CafeController.java
@@ -4,18 +4,24 @@ package com.coffee.backend.domain.cafe.controller;
 import com.coffee.backend.domain.auth.controller.AuthenticationPrincipal;
 import com.coffee.backend.domain.cafe.dto.CafeDto;
 import com.coffee.backend.domain.cafe.service.CafePublisher;
+import com.coffee.backend.domain.cafe.service.CafeService;
 import com.coffee.backend.domain.user.entity.User;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
 @Slf4j
 public class CafeController {
     private final CafePublisher cafePublisher;
+    private final CafeService cafeService;
 
     /*
     클라이언트에서 서버로 메시지 전송
@@ -26,4 +32,12 @@ public class CafeController {
         log.info("Message Catch!!");
         cafePublisher.updateCafeChoice(dto);
     }
+
+    //TODO : cafeId를 입력으로 받아서 특정 카페에 속한 모든 유저들을 redis에서 찾아 프론트로 넘긴다.
+    @GetMapping("/cafe/{cafeId}") //http://localhost:8080/cafe/1
+    public ResponseEntity<Set<Object>> getCafeUsers(@PathVariable String cafeId) {
+        System.out.println("cafeId = " + cafeId);
+        return ResponseEntity.ok(cafeService.getUsersByCafeId(cafeId));
+    }
+
 }
---
+++ backend/src/main/java/com/coffee/backend/domain/cafe/service/CafeService.java
@@ -0,0 +1,62 @@
+package com.coffee.backend.domain.cafe.service;
+
+import java.util.Set;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class CafeService {
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    //TODO : CafeController.java 의 sendMatchRequest() 에서 호출됨 테스트용 데이터가 잘 조회되는지? 어떻게 조회되는지?
+    public String getCafeByUserId(String userId) {
+        // redis 에서 userId로 cafeId 조회
+        Object cafeId = redisTemplate.opsForValue().get(userId.toString());
+        System.out.println("userId= " + userId + " cafeId = " + cafeId); // userId = user1, cafeId = 1
+        return (String) cafeId;
+    }
+
+//    public void getUsersByCafeId(Long cafeId) {
+//        // TODO : 그 cafeId 를 가진 유저를 redis 에서 싹 조회하고
+//        Set<String> userSet = getUserListFromRedis(cafeId);
+//        // TODO : userSet에서 각 userId 꺼내서 그 userId로 DB에서 User entity, (Company entity, position entity) 를 조회
+//        아래 형식처럼 list에 담아 리턴
+//        /*
+//        리턴 형식 (companyName, positionName은 일단 NULL로 보낸다.)
+//            {
+//                "userList" : [
+//                {
+//                "userId" : Long
+//                "nickname" : String
+//                "introduction" : String
+//                "companyName" : String
+//                "positionName" : String
+//                },
+//                {
+//                    동일하게
+//                },
+//                ]
+//            }
+//         */
+//    }
+//
+
+    // redis 에서 cafeId로 조회해서 userList 리턴
+    public Set<Object> getUsersByCafeId(String cafeId) {
+        String cafeChoiceKey = "cafe:" + cafeId;
+        Set<Object> userSet = redisTemplate.opsForSet().members(cafeChoiceKey); // 카페 ID에 해당하는 세트의 모든 사용자 ID 조회
+        System.out.println("cafeId = " + cafeId + " userSet = " + userSet); // 출력
+        return userSet;
+    }
+
+//    //
+//    // userId로 User entity, Company entity, Position entity 조회
+//    public void getUserInfoFromDB(String userId) {
+//        User user = userRepository.findById(userId);
+//        Company company = companyRepository.findById(user.getCompanyId()); //아직 company entity 없음
+//    }
+}
