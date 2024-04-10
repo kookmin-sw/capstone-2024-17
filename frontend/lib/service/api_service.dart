480efd6ad75464a6155b54c31b25c095fe5473b6
godeka <godeka@naver.com>
2024-04-10 14:28:59 +0900

    [refactor] baseUrl 분리 #53

--- frontend/lib/service/api_service.dart
+++ frontend/lib/service/api_service.dart
@@ -3,11 +3,13 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:frontend/model/user_model.dart';
 
+// 임의로 baseUrl 지정 (추후 서버 주소로 변경 필요)
+const baseUrl = "https://localhost:8080";
+
 // 카페에 있는 유저 목록 GET 요청
 Future<List<UserModel>> getUserList(String cafeId) async {
   List<UserModel> userList = [];
-  final url = Uri.parse("https://localhost:8080/cafe/$cafeId");
-
+  final url = Uri.parse("$baseUrl/cafe/$cafeId");
   final response = await http.get(url);
 
   if (response.statusCode == 200) {
