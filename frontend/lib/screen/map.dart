7582856791dfcdfe51a13938ff670a9ced5ce3c7
Gongchaeyeon <dnjftjd53@kookmin.ac.kr>
2024-04-10 21:54:33 +0900

    [Feat] #30 주변 카페 정보 불러오기

--- frontend/lib/screen/map.dart
+++ frontend/lib/screen/map.dart
@@ -17,23 +17,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoApp(
-      home: map_widget(),
+      home: MapWidget(),
     );
   }
 }
 
-class map_widget extends StatefulWidget {
-  const map_widget({Key? key, required}) : super(key: key);
-
+class MapWidget extends StatefulWidget {
   @override
-  State<map_widget> createState() => _map_widgetState();
+  _MapWidgetState createState() => _MapWidgetState();
 }
 
-class _map_widgetState extends State<map_widget> {
+class _MapWidgetState extends State<MapWidget> {
+
   @override
   void initState() {
     super.initState();
-    requestLocationPermission();
+    requestLocationPermission(); //위치 권한 여부
   }
 
   late GoogleMapController _controller;
@@ -99,9 +98,8 @@ class _map_widgetState extends State<map_widget> {
       child: Stack(
         children: [
           GoogleMap(
-        mapType: MapType.terrain,
-
-initialCameraPosition: CameraPosition(
+            mapType: MapType.terrain,
+            initialCameraPosition: CameraPosition(
               target: LatLng(37.5925683, 127.0164784), //성신여대 입구
               zoom: 18,
             ),
@@ -112,8 +110,8 @@ initialCameraPosition: CameraPosition(
                 markerId: MarkerId(place['name']),
                 position: LatLng(place['latitude'], place['longitude']),
                 infoWindow: InfoWindow(title: place['name']),
-                onTap: () { //마커 클릭 시 이동하는 곳
-                  Navigator.push( 
+                onTap: () {
+                  Navigator.push(
                       context,
                       CupertinoPageRoute(
                           builder: (context) => MarkerDetailScreen()));
@@ -125,62 +123,29 @@ initialCameraPosition: CameraPosition(
           Positioned(
             bottom: 25,
             left: 16,
-            child: FloatingActionButton( //현재 위치 표시하는 버튼
-              onPressed: _getCurrentLocation, //현재 위치 구하는 함수로 이동
+            child: FloatingActionButton(
+              onPressed: _getCurrentLocation,
               foregroundColor: Colors.black,
               backgroundColor: Colors.white,
-              elevation: 8, // 그림자 크기
+              elevation: 8,
               child: Icon(Icons.my_location),
               shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10), // 현재 버튼 모서리 둥글기
+                borderRadius: BorderRadius.circular(10),
               ),
             ),
           ),
-          // Positioned(
-          //   top: 54,
-          //   child: CupertinoButton(
-          //     onPressed: () {
-          //       Navigator.push( //상단바의 위치검색 클릭 시 이동
-          //           context,
-          //           CupertinoPageRoute(
-          //               builder: (context) => MarkerDetailScreen()));
-          //     },
-          //     child: Container(
-          //       width: MediaQuery.of(context).size.width - 21,
-          //       height: 44,
-          //       child: Row(
-          //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          //         children: [
-          //           Padding(
-          //             padding: const EdgeInsets.only(left: 16.0),
-          //             child: Text(
-          //               '위치 검색',
-          //               style: const TextStyle(
-          //                 fontSize: 18,
-          //                 fontWeight: FontWeight.w600,
-          //                 color: Colors.grey,
-          //               ),
-          //             ),
-          //           ),
-          //           const Spacer(),
-          //           Padding(
-          //             padding: const EdgeInsets.only(right: 13.0),
-          //           ),
-          //         ],
-          //       ),
-          //       decoration: BoxDecoration( // 위치 검색 버튼 꾸미는 곳
-          //           color: Colors.white,
-          //           borderRadius: BorderRadius.all(Radius.circular(8))),
-          //     ),
-          //   ),
-          // ),
         ],
       ),
     );
   }
 }
 
-class MarkerDetailScreen extends StatelessWidget {
+class MarkerDetailScreen extends StatefulWidget {
+  @override
+  _MarkerDetailScreenState createState() => _MarkerDetailScreenState();
+}
+
+class _MarkerDetailScreenState extends State<MarkerDetailScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -193,12 +158,3 @@ class MarkerDetailScreen extends StatelessWidget {
     );
   }
 }
-
-// class marker_widget extends StatelessWidget {
-//   const marker_widget({Key? key}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return const Placeholder();
-//   }
-// }
