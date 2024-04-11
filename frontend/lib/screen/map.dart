363a150984361e13929dc207d13ca227c30f9897
Gongchaeyeon <dnjftjd53@kookmin.ac.kr>
2024-04-11 17:52:47 +0900

    [refactor] #4 카페 정보 불러오기, 현재 위치로 이동 기능 mapPlace.dart에 병합

--- frontend/lib/screen/map.dart
+++ frontend/lib/screen/map.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -33,6 +36,8 @@ class _MapWidgetState extends State<MapWidget> {
   void initState() {
     super.initState();
     requestLocationPermission(); //위치 권한 여부
+    _searchcafes(const LatLng(37.5925683, 127.0164784)); // 초기 위치에 대한 카페 검색
+
   }
 
   late GoogleMapController _controller;
@@ -65,35 +70,46 @@ class _MapWidgetState extends State<MapWidget> {
     });
   }
 
+  Set<Marker> _markers = {};
+
+  Future<void> _searchcafes(LatLng position) async {
+    // try{
+    final response = await http.get(Uri.parse(
+        'https://maps.googleapis.com/maps/api/place/textsearch/json?query=(카페,커피숍)&location=${position.latitude},${position.longitude}&radius=1500&key=${dotenv.env['googleApiKey']}'));
+
+    if (response.statusCode == 200) {
+      print("성공");
+      print("Response Body: ${response.body}");
+      final data = json.decode(response.body);
+      _setMarkers(data['results']);
+
+    } else {
+      print("실패");
+      throw Exception('Failed to load cafe');
+    }
+  }
+
+
+  // cafe 마커표시하고 누르면 cafe 이름보여줌
+  void _setMarkers(List<dynamic> places) {
+    final Set<Marker> localMarkers = {};
+    for (var place in places) {
+      localMarkers.add(Marker(
+        markerId: MarkerId(place['place_id']),
+        position: LatLng(place['geometry']['location']['lat'],
+            place['geometry']['location']['lng']),
+        infoWindow: InfoWindow(title: place['name']),
+      ));
+    }
+
+    setState(() {
+      _markers = localMarkers;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    List<Map<String, dynamic>> places = [
-      {
-        "name": "카페아일랜드",
-        "latitude": 37.5925414,
-        "longitude": 127.0170871,
-      },
-      {
-        "name": "카페핸드",
-        "latitude": 37.593260320178935,
-        "longitude": 127.01515591535698,
-      },
-      {
-        "name": "펠어커피",
-        "latitude": 37.59324683702535,
-        "longitude": 127.01490680535643,
-      },
-      {
-        "name": "멜랑슈 에스프레소",
-        "latitude": 37.593311913011235,
-        "longitude": 127.01674115525596,
-      },
-      {
-        "name": "투썸플레이스",
-        "latitude": 37.59354169227075,
-        "longitude": 127.01655437554344,
-      },
-    ];
+
     return CupertinoPageScaffold(
       child: Stack(
         children: [
@@ -101,23 +117,11 @@ class _MapWidgetState extends State<MapWidget> {
             mapType: MapType.terrain,
             initialCameraPosition: CameraPosition(
               target: LatLng(37.5925683, 127.0164784), //성신여대 입구
-              zoom: 18,
+              zoom: 15,
             ),
             myLocationEnabled: _myLocationEnabled,
             myLocationButtonEnabled: false,
-            markers: Set<Marker>.of(places.map((place) {
-              return Marker(
-                markerId: MarkerId(place['name']),
-                position: LatLng(place['latitude'], place['longitude']),
-                infoWindow: InfoWindow(title: place['name']),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      CupertinoPageRoute(
-                          builder: (context) => MarkerDetailScreen()));
-                },
-              );
-            })),
+            markers: _markers,
             onMapCreated: (controller) => _controller = controller,
           ),
           Positioned(
