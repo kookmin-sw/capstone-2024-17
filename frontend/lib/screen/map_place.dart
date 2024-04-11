de65d0652282964c542b6abaea243f62017131f1
Gongchaeyeon <dnjftjd53@kookmin.ac.kr>
2024-04-12 02:17:14 +0900

    [feat] #75 카페 마커 커스텀(임시 Label: 카페 이름)

--- frontend/lib/screen/map_place.dart
+++ frontend/lib/screen/map_place.dart
@@ -1,3 +1,6 @@
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'dart:convert';
@@ -104,15 +107,49 @@ class _GoogleMapWidgetState extends State<Google_Map> {
 
   }
 
+
+  Future<Uint8List> _createMarkerImage(String label) async {
+    final recorder = ui.PictureRecorder();
+    final canvas = Canvas(recorder, Rect.fromPoints(Offset(0.0, 0.0), Offset(160.0, 160.0))); // Canvas 크기를 100x100으로 변경
+
+    // 마커 아이콘을 그리는 코드
+    final paint = Paint()..color = Color.fromRGBO(246, 82, 16, 1); //red, green, blue, opacity
+    canvas.drawCircle(Offset(80, 80), 80, paint); // 중심(80, 80), 반지름 80
+
+    // 텍스트 크기 계산 (중앙배치 하기 위함)
+    final textStyle = TextStyle(color: Colors.white, fontSize: 30); // 폰트, 크기
+    final textSpan = TextSpan(text: label, style: textStyle); // 마진
+    final textPainter = TextPainter(text: textSpan, textDirection: TextDirection.ltr);
+    textPainter.layout();
+
+    // 텍스트 중앙 배치
+    final textOffset = Offset(80 - textPainter.width / 2, 80 - textPainter.height / 2); // 텍스트의 시작 위치 계산
+    textPainter.paint(canvas, textOffset); // 텍스트 뿌림
+
+    final picture = recorder.endRecording();
+    final img = await picture.toImage(160, 160); // 이미지 크기 = 원의 크기
+    final byteData = await img.toByteData(format: ui.ImageByteFormat.png);
+    return byteData!.buffer.asUint8List();
+  }
+
+
   // cafe 마커표시하고 누르면 cafe 이름보여줌
-  void _setMarkers(List<dynamic> places) {
+  void _setMarkers(List<dynamic> places) async {
     final Set<Marker> localMarkers = {};
     for (var place in places) {
+      // 여기서 라벨에 텍스트 명 변경가능
+      final markerIcon = await _createMarkerImage(place['name']); // 여기서 라벨에 텍스트 명 변경가능
+
       localMarkers.add(Marker(
         markerId: MarkerId(place['place_id']),
-        position: LatLng(place['geometry']['location']['lat'],
-            place['geometry']['location']['lng']),
-        infoWindow: InfoWindow(title: place['name']),
+        position: LatLng(
+          place['geometry']['location']['lat'],
+          place['geometry']['location']['lng'],
+        ),
+        icon: BitmapDescriptor.fromBytes(markerIcon), // 라벨을 포함한 마커 아이콘 설정
+        infoWindow: InfoWindow(
+          title: place['name'],
+        ),
       ));
     }
 
@@ -120,6 +157,7 @@ class _GoogleMapWidgetState extends State<Google_Map> {
       _markers = localMarkers;
     });
   }
+
 
   void _setCircle(LatLng position){
     Set<Circle> localcircles = {};
