84961e42bd80ef80edf7502e55b02e91d08954fb
godeka <godeka@naver.com>
2024-04-14 19:57:30 +0900

    [feat] 반경 내 모든 카페 유저목록 가져오는 post 함수로 수정 #85

--- frontend/lib/service/api_service.dart
+++ frontend/lib/service/api_service.dart
@@ -6,20 +6,25 @@ import 'package:frontend/model/user_model.dart';
 // 임의로 baseUrl 지정 (추후 서버 주소로 변경 필요)
 const baseUrl = "https://localhost:8080";
 
-// 카페에 있는 유저 목록 GET 요청
-Future<List<UserModel>> getUserList(String cafeId) async {
-  List<UserModel> userList = [];
-  final url = Uri.parse("$baseUrl/cafe/$cafeId");
-  final response = await http.get(url);
+// 주변 카페에 있는 유저 목록 POST 요청으로 받아오기
+Future<Map<String, List<UserModel>>> getAllUsers(List<String> cafeList) async {
+  final url = Uri.parse("$baseUrl/cafe");
+  final response = await http.post(
+    url,
+    headers: {"Content-Type": "application/json"},
+    body: jsonEncode({cafeList: cafeList}),
+  );
 
   if (response.statusCode == 200) {
-    List<dynamic> users = jsonDecode(response.body);
+    Map<String, List<UserModel>> allUsers = {};
+    Map<String, dynamic> jsonResult = jsonDecode(response.body);
 
-    for (var user in users) {
-      final userModel = UserModel.fromJson(user);
-      userList.add(userModel);
-    }
-    return userList;
+    jsonResult.forEach((cafe, userList) {
+      allUsers[cafe] =
+          userList.map((user) => UserModel.fromJson(user)).toList();
+    });
+
+    return allUsers;
   }
   throw Error();
 }
