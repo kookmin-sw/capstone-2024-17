638a85f77b780807880585e37ab6c33861c8a04d
godeka <godeka@naver.com>
2024-04-15 14:34:48 +0900

    [feat] getAllUsers api 호출하여 allUsers provider로 전달 #85

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,24 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:frontend/model/user_model.dart';
 import 'package:frontend/screen/signup_screen.dart';
 import 'package:frontend/screen/user_screen.dart';
+import 'package:frontend/service/api_service.dart';
 import 'package:provider/provider.dart';
 import 'package:frontend/login_view_model.dart';
 import 'package:frontend/screen/login_screen.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
+const List<String> sampleCafeList = [
+  "cafe-1",
+  "cafe-2",
+  "cafe-3",
+  "cafe-4",
+  "cafe-5",
+];
+
 void main() async {
+  Map<String, List<UserModel>>? allUsers = await getAllUsers(sampleCafeList);
+
   await dotenv.load();
   KakaoSdk.init(
     nativeAppKey: dotenv.env['KAKAO_NATIVE_APP_KEY'],
     // 웹으로 실행하는 경우 필요한 키
     javaScriptAppKey: dotenv.env['JAVA_SCRIPT_APP_KEY'],
   );
-  runApp(MultiProvider(providers: [
-    ChangeNotifierProvider(
-        create: (_) =>
-            LoginViewModel(user: null)), // provider로 감싸고 유저의 초기값 넣어주기
-  ], child: const MyApp()));
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => LoginViewModel(user: null),
+        ), // provider로 감싸고 유저의 초기값 넣어주기
+        Provider(
+          create: (context) => allUsers,
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
