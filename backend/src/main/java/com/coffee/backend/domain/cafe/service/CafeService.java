82dd275581c2bf01cbbb6262abe19b06d8d780e6
hyeesw <hyeeun7904@kookmin.ac.kr>
2024-04-17 00:08:23 +0900

    [feature] #52 카페선택 Redis add, delete 로직

--- backend/src/main/java/com/coffee/backend/domain/cafe/service/CafeService.java
+++ backend/src/main/java/com/coffee/backend/domain/cafe/service/CafeService.java
@@ -21,6 +21,33 @@ public class CafeService {
     private final RedisTemplate<String, Object> redisTemplate;
     private final UserService userService;
 
+    //redis에 add 하는 메소드
+    public void addCafeChoice(String cafeId, String loginId) {
+        System.out.println("26 : 여기 오긴 했는데");
+        /*
+        Redis에 아래 형식으로 저장됨
+            namespace = cafe
+            key = starbucks
+            value = set(user1, user2, user3)
+         */
+        String cafeChoiceKey = "cafe:" + cafeId;
+        redisTemplate.opsForSet().add(cafeChoiceKey, loginId); // 카페 ID에 해당하는 세트에 사용자 ID 추가
+    }
+
+    //redis에서 delete 하는 메소드
+    public void deleteCafeChoice(String cafeId, String loginId) {
+        final String cafeChoiceKey = "cafe:" + cafeId;
+
+        // 카페 ID에 해당하는 세트에서 사용자 ID 찾기
+        Boolean isMember = redisTemplate.opsForSet().isMember(cafeChoiceKey, loginId);
+        if (Boolean.TRUE.equals(isMember)) {
+            redisTemplate.opsForSet().remove(cafeChoiceKey, loginId);
+        } else {
+            throw new IllegalArgumentException("User ID '" + loginId + "' 는 '" + cafeId + "' 카페에 속해 있지 않습니다.");
+        }
+    }
+
+
     public List<CafeUserProfileDto> getUserProfilesFromRedisAndDB(String cafeId) {
         // cafeId 를 가진 유저를 redis 에서 싹 조회
         Set<Object> userSet = getUserListFromRedis(cafeId);
