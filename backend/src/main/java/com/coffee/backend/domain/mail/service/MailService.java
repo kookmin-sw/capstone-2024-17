d1b037af919e4d6629f5231b28d6e2e961097348
haram8009 <haram8009@gmail.com>
2024-04-16 22:00:00 +0900

    [fix] #11 이메일 html 형태 가능하게 서비스 코드 수정

--- backend/src/main/java/com/coffee/backend/domain/mail/service/MailService.java
+++ backend/src/main/java/com/coffee/backend/domain/mail/service/MailService.java
@@ -2,10 +2,13 @@ package com.coffee.backend.domain.mail.service;
 
 import com.coffee.backend.exception.CustomException;
 import com.coffee.backend.exception.ErrorCode;
+import jakarta.mail.Message.RecipientType;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
@@ -15,30 +18,25 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class MailService {
 
+    @Value("${spring.mail.username}")
+    private String fromEmail;
     private final JavaMailSender emailSender;
+
 
     public void sendEmail(String toEmail,
                           String title,
                           String text) {
-        SimpleMailMessage emailForm = createEmailForm(toEmail, title, text);
+        MimeMessage message = emailSender.createMimeMessage();
         try {
-            emailSender.send(emailForm);
-        } catch (RuntimeException e) {
+            message.setFrom(new InternetAddress(fromEmail, "커리어 한잔"));
+            message.addRecipients(RecipientType.TO, toEmail);
+            message.setSubject(title);
+            message.setText(text, "UTF-8", "html");
+            emailSender.send(message);
+        } catch (Exception e) {
             log.info("MailService.sendEmail exception occur toEmail: {}, " +
                     "title: {}, text: {}", toEmail, title, text);
             throw new CustomException(ErrorCode.UNABLE_TO_SEND_EMAIL_ERROR);
         }
-    }
-
-    // 발신할 이메일 데이터 세팅
-    private SimpleMailMessage createEmailForm(String toEmail,
-                                              String title,
-                                              String text) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(toEmail);
-        message.setSubject(title);
-        message.setText(text);
-
-        return message;
     }
 }
