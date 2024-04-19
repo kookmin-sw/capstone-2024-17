99850f4cea579520c3700707f603e82ca34fd3be
Gongchaeyeon <dnjftjd53@kookmin.ac.kr>
2024-04-20 04:22:33 +0900

    [feat] cafe_details에 영업상태, takeout, 매장내식사 정보 추가 #92

--- frontend/lib/widgets/cafe_info.dart
+++ frontend/lib/widgets/cafe_info.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CafeInfo extends StatelessWidget {
@@ -23,8 +24,60 @@ class CafeInfo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool isOpen = cafeOpen == 'true'; // 예를 선택하면 true, 그렇지 않으면 false
+    bool isDineIn = cafeDineIn == 'true';
+    bool isTakeout = cafeTakeout == 'true';
+
     return Column(
+
       children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(width: 50),
+            Icon(
+              isDineIn ? Icons.fastfood_outlined : Icons.no_meals_sharp ,
+              size: 20,
+              color: isDineIn ? Colors.green : Colors.red,
+            ),
+            const SizedBox(width: 10),
+            SizedBox(
+              width: 120,
+              child: Text(
+                '매장 내 식사',
+                style: const TextStyle(fontSize: 13),
+                overflow: TextOverflow.clip,
+              ),
+            ),
+            Icon(
+              isTakeout ? Icons.wallet_giftcard_sharp : Icons.not_interested ,
+              size: 20,
+              color: isDineIn ? Colors.green : Colors.red,
+            ),
+            const SizedBox(width: 10),
+            SizedBox(
+              width: 100,
+              child: Text(
+                '테이크아웃',
+                style: const TextStyle(fontSize: 13),
+                overflow: TextOverflow.clip,
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height:10),
+        Row(
+          children: [
+            // 이전 위젯들
+            Container(
+              width: 360, // 네모 박스의 가로 길이
+              height: 1, // 네모 박스의 세로 길이
+              color: Colors.grey , // 네모 박스의 색상
+            ),
+            // 이후에 추가할 위젯들
+          ],
+        ),
+        SizedBox(height:15),
         Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -43,6 +96,7 @@ class CafeInfo extends StatelessWidget {
             ),
           ],
         ),
+        SizedBox(height:15),
         Row(
           children: [
             const Icon(
@@ -59,6 +113,8 @@ class CafeInfo extends StatelessWidget {
             ),
           ],
         ),
+        SizedBox(height:15),
+
         Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -66,6 +122,23 @@ class CafeInfo extends StatelessWidget {
               Icons.schedule,
               size: 25,
             ),
+            const SizedBox(width: 20),
+            Text(
+              isOpen? '영업중':'영업종료',
+              style: TextStyle(
+                fontSize: 20,
+                color: isOpen ? Colors.green : Colors.red, // cafeOpen이 true이면 초록색, 그렇지 않으면 검은색으로 설정
+              ),
+            ),
+            const SizedBox(width: 20),
+          ],
+        ),
+
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+
+            const SizedBox(width: 25),
             const SizedBox(width: 20),
             Text(
               businessHours,
