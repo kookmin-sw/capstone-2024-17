6d14b18bcc86b14302cfc5d7ddc02188a8571a1e
godeka <godeka@naver.com>
2024-04-20 15:37:22 +0900

    [fix] post 요청 시 헤더에 authorization 추가 (임의로 고정값 토큰 전달) #103

--- frontend/lib/service/api_service.dart
+++ frontend/lib/service/api_service.dart
@@ -4,13 +4,18 @@ import 'package:http/http.dart' as http;
 import 'package:frontend/model/user_model.dart';
 
 const baseUrl = "http://43.203.218.27:8080";
+const userToken =
+    "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJBY2Nlc3NUb2tlbiIsImV4cCI6MTcxMzU5OTA5NiwiaWQiOjF9.HSC3z5gus1gM0DavxjZdhVBZSlUCGhgEbjIYS2-bKng";
 
 // 주변 카페에 있는 유저 목록 POST 요청으로 받아오기
 Future<Map<String, List<UserModel>>> getAllUsers(List<String> cafeList) async {
   final url = Uri.parse("$baseUrl/cafe/get-users");
   final response = await http.post(
     url,
-    headers: {"Content-Type": "application/json"},
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer $userToken",
+    },
     body: jsonEncode({"cafeList": cafeList}),
   );
 
@@ -52,22 +57,21 @@ Future<Map<String, dynamic>> signup(String loginId, String password,
 
 // 로그인
 Future<Map<String, dynamic>> login(
-    String loginId,
-    String password,
-  ) async {
-    final url = Uri.parse('$baseUrl/auth/signIn');
-    final data = jsonEncode({
-      'loginId': loginId,
-      'password': password,
-    });
-    try {
-      http.Response res = await http.post(url,
-          headers: {"Content-Type": "application/json"}, body: data);
-      Map<String, dynamic> jsonData = jsonDecode(res.body);
-      return jsonData;
-    } catch (error) {
-      print('error: $error');
-      throw Error();
-    }
+  String loginId,
+  String password,
+) async {
+  final url = Uri.parse('$baseUrl/auth/signIn');
+  final data = jsonEncode({
+    'loginId': loginId,
+    'password': password,
+  });
+  try {
+    http.Response res = await http.post(url,
+        headers: {"Content-Type": "application/json"}, body: data);
+    Map<String, dynamic> jsonData = jsonDecode(res.body);
+    return jsonData;
+  } catch (error) {
+    print('error: $error');
+    throw Error();
   }
-
+}
