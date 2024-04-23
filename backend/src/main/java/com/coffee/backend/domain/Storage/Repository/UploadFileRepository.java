3a33f28391a2cbd1c6db6b18e87002838dc9fb3e
godeka <godeka@naver.com>
2024-04-23 14:17:20 +0900

    [remove] 백엔드 storage 폴더 삭제

--- backend/src/main/java/com/coffee/backend/domain/Storage/Repository/UploadFileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.coffee.backend.domain.Storage.Repository;
-
-import com.coffee.backend.domain.Storage.entity.UploadFile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UploadFileRepository extends JpaRepository<UploadFile, Long> {
-}
