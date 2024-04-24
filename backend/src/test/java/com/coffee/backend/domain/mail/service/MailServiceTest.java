b2f7aa454159de48b87afd88d4fb89470995db6e
Haeeun Kim <84231143+hyeesw@users.noreply.github.com>
2024-04-24 18:48:50 +0900

    [Release] #13 test 코드 주석 -  MailServiceTest.java

--- backend/src/test/java/com/coffee/backend/domain/mail/service/MailServiceTest.java
+++ backend/src/test/java/com/coffee/backend/domain/mail/service/MailServiceTest.java
@@ -1,37 +1,37 @@
-package com.coffee.backend.domain.mail.service;
+// package com.coffee.backend.domain.mail.service;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+// import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest
-@Transactional
-class MailServiceTest {
-    @Autowired
-    private MailService mailService;
+// @SpringBootTest
+// @Transactional
+// class MailServiceTest {
+//     @Autowired
+//     private MailService mailService;
 
-    @Test
-    void sendEmail() throws Exception {
-        // given
-        String toEmail = "haram8009@gmail.com";
-        String title = "제목입니다";
-        String text = "텍스트";
+//     @Test
+//     void sendEmail() throws Exception {
+//         // given
+//         String toEmail = "haram8009@gmail.com";
+//         String title = "제목입니다";
+//         String text = "텍스트";
 
-        Boolean done = Boolean.TRUE;
+//         Boolean done = Boolean.TRUE;
 
-        // when
-        try {
-            mailService.sendEmail(toEmail, title, text);
-        } catch (Exception e) {
-            done = Boolean.FALSE;
-            System.out.println("\n\n" + e.getMessage() + "\n\n");
-        }
+//         // when
+//         try {
+//             mailService.sendEmail(toEmail, title, text);
+//         } catch (Exception e) {
+//             done = Boolean.FALSE;
+//             System.out.println("\n\n" + e.getMessage() + "\n\n");
+//         }
 
-        // then
-        assertTrue(done);
+//         // then
+//         assertTrue(done);
 
-    }
-}
+//     }
+// }
