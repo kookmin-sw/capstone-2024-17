79c96c93bf740a0d9be419c268a7a07ebb9c2bc0
godeka <godeka@naver.com>
2024-04-27 13:34:00 +0900

    [feat] 받는 사람 프로필 부분 추가 #112

--- frontend/lib/screen/coffeechat_req_list.dart
+++ frontend/lib/screen/coffeechat_req_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/widgets/bottom_text_button.dart';
 import 'package:frontend/widgets/color_text_container.dart';
+import 'package:frontend/widgets/profile_img.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,40 +24,41 @@ class CoffeechatReqList extends StatelessWidget {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
-          appBar: PreferredSize(
-            preferredSize: const Size.fromHeight(70),
-            child: AppBar(
-              title: const Text(
-                "커피챗 요청 목록",
-                style: TextStyle(fontSize: 24),
-              ),
-              toolbarHeight: 100,
-              backgroundColor: Colors.white,
-              leading: const Icon(Icons.arrow_back_ios),
-              leadingWidth: 70,
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(70),
+          child: AppBar(
+            title: const Text(
+              "커피챗 요청 목록",
+              style: TextStyle(fontSize: 24),
             ),
+            toolbarHeight: 100,
+            backgroundColor: Colors.white,
+            leading: const Icon(Icons.arrow_back_ios),
+            leadingWidth: 70,
           ),
-          body: Column(
-            children: [
-              const TabBar(tabs: [
-                Tab(text: '보낸 요청'),
-                Tab(text: '받은 요청'),
+        ),
+        body: Column(
+          children: [
+            const TabBar(tabs: [
+              Tab(text: '보낸 요청'),
+              Tab(text: '받은 요청'),
+            ]),
+            Expanded(
+              child: TabBarView(children: [
+                const SentReqList(),
+                ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return const ListTile(
+                      title: Text('받은 요청'),
+                    );
+                  },
+                ),
               ]),
-              Expanded(
-                child: TabBarView(children: [
-                  const SentReqList(),
-                  ListView.builder(
-                    itemCount: 10,
-                    itemBuilder: (context, index) {
-                      return const ListTile(
-                        title: Text('받은 요청'),
-                      );
-                    },
-                  ),
-                ]),
-              ),
-            ],
-          )),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -68,7 +70,13 @@ class SentReqList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        const Text("프로필"),
+        const ReceiverProfile(
+          nickname: "goodnavers",
+          company: "네이버",
+          position: "데이터 엔지니어링",
+          introduction:
+              "안녕하세요, goodnavers 입니다. 편하게 커피챗 걸어주세요. 어쩌고 저쩌고 블라블라블라블라블라블라블라블라블라",
+        ),
         const ColorTextContainer(text: "# 당신의 업무가 궁금해요."),
         const Text(
           "자동 취소까지 남은 시간\n09:59",
@@ -76,6 +84,88 @@ class SentReqList extends StatelessWidget {
         ),
         BottomTextButton(text: "요청 취소하기", handlePressed: () {}),
       ],
+    );
+  }
+}
+
+class ReceiverProfile extends StatelessWidget {
+  final String nickname;
+  final String company;
+  final String position;
+  final String introduction;
+
+  const ReceiverProfile({
+    super.key,
+    required this.nickname,
+    required this.company,
+    required this.position,
+    required this.introduction,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 20),
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+      width: 370,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(color: Colors.grey, width: 1),
+      ),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              const SizedBox(
+                width: 35,
+              ),
+              const ProfileImg(logo: "assets/coffee_bean.png"),
+              const SizedBox(
+                width: 35,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    nickname,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    company,
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                  Text(
+                    position,
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+            width: 280,
+            height: 150,
+            margin: const EdgeInsets.all(10),
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: SingleChildScrollView(
+              child: Text(
+                introduction,
+                style: const TextStyle(fontSize: 18),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
