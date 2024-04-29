0530a0ee6d9f7d5abfac22dccd32f90b4c26f593
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2024-04-30 00:39:21 +0900

    [refactor] 매칭 Dto 수정

--- backend/src/main/java/com/coffee/backend/domain/match/dto/MatchRequestDto.java
+++ backend/src/main/java/com/coffee/backend/domain/match/dto/MatchRequestDto.java
@@ -6,7 +6,6 @@ import lombok.Setter;
 @Getter
 @Setter
 public class MatchRequestDto {
-    private String senderId;
-    private String receiverId;
-    private String targetToken; // 푸시 알림 위해 필요
+    private Long senderId;
+    private Long receiverId;
 }
