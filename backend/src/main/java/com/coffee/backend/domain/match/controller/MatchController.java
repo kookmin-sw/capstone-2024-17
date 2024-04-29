b00e30f7f7d048be4fc4bd874ca6d96d06e79736
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2024-04-30 00:40:56 +0900

    [refactor] 매칭 요청, 수락, 거절, 취소 전격 리팩토링

--- backend/src/main/java/com/coffee/backend/domain/match/controller/MatchController.java
+++ backend/src/main/java/com/coffee/backend/domain/match/controller/MatchController.java
@@ -1,17 +1,18 @@
 package com.coffee.backend.domain.match.controller;
 
-import com.coffee.backend.domain.auth.controller.AuthenticationPrincipal;
 import com.coffee.backend.domain.match.dto.MatchDto;
+import com.coffee.backend.domain.match.dto.MatchIdDto;
 import com.coffee.backend.domain.match.dto.MatchRequestDto;
 import com.coffee.backend.domain.match.dto.ReviewDto;
 import com.coffee.backend.domain.match.entity.Review;
 import com.coffee.backend.domain.match.service.MatchService;
-import com.coffee.backend.domain.user.entity.User;
 import com.coffee.backend.utils.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,32 +25,28 @@ public class MatchController {
     private final MatchService matchService;
 
     @PostMapping("/request")
-    public ResponseEntity<ApiResponse<MatchDto>> sendMatchRequest(@AuthenticationPrincipal User user,
-                                                                  @RequestBody MatchRequestDto dto) {
+    public ResponseEntity<ApiResponse<MatchDto>> sendMatchRequest(@RequestBody MatchRequestDto dto) {
         log.info("Request Message Catch!!");
         MatchDto response = matchService.sendMatchRequest(dto);
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 
-    @PostMapping("/accept")
-    public ResponseEntity<ApiResponse<MatchDto>> acceptMatchRequest(@AuthenticationPrincipal User user,
-                                                                    @RequestBody MatchDto dto) {
+    @PutMapping("/accept")
+    public ResponseEntity<ApiResponse<MatchDto>> acceptMatchRequest(@RequestBody MatchIdDto dto) {
         log.info("Accept Message Catch!!");
         MatchDto response = matchService.acceptMatchRequest(dto);
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 
-    @PostMapping("/decline")
-    public ResponseEntity<ApiResponse<MatchDto>> declineMatchRequest(@AuthenticationPrincipal User user,
-                                                                     @RequestBody MatchDto dto) {
+    @DeleteMapping("/decline")
+    public ResponseEntity<ApiResponse<MatchDto>> declineMatchRequest(@RequestBody MatchIdDto dto) {
         log.info("Decline Message Catch!!");
         MatchDto response = matchService.declineMatchRequest(dto);
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 
-    @PostMapping("/cancel")
-    public ResponseEntity<ApiResponse<MatchDto>> cancelMatchRequest(@AuthenticationPrincipal User user,
-                                                                    @RequestBody MatchDto dto) {
+    @DeleteMapping("/cancel")
+    public ResponseEntity<ApiResponse<MatchDto>> cancelMatchRequest(@RequestBody MatchIdDto dto) {
         log.info("Cancel Message Catch!!");
         MatchDto response = matchService.cancelMatchRequest(dto);
         return ResponseEntity.ok(ApiResponse.success(response));
@@ -57,7 +54,7 @@ public class MatchController {
 
     @PostMapping("/review")
     public ResponseEntity<ApiResponse<Review>> submitReview(@RequestBody ReviewDto dto) {
-        Review response = matchService.submitReview(dto);
+        Review response = matchService.saveReview(dto);
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 }
