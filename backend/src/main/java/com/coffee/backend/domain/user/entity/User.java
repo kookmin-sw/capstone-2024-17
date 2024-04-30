791551806d3e745e8ba9e887adc9180ef8db07e5
haram8009 <haram8009@gmail.com>
2024-04-30 21:50:05 +0900

    [fix] 유저 테이블에 company 다대일 관계 필드 추가 #11

--- backend/src/main/java/com/coffee/backend/domain/user/entity/User.java
+++ backend/src/main/java/com/coffee/backend/domain/user/entity/User.java
@@ -1,9 +1,12 @@
 package com.coffee.backend.domain.user.entity;
 
+import com.coffee.backend.domain.company.entity.Company;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -22,4 +25,7 @@ public class User {
     private String email;
     private String phone;
     private String userUUID;
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private Company companyId;
 }
