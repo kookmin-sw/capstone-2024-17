8af0dcebabf0a9993f38b3c02c39e7a6f7a59502
godeka <godeka@naver.com>
2024-05-01 16:28:24 +0900

    [feat] 버튼 포함 앱바 추가 #137

--- frontend/lib/widgets/top_appbar.dart
+++ frontend/lib/widgets/top_appbar.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 
 class TopAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
+  final List<Widget>? buttons;
   @override
   final Size preferredSize;
 
   const TopAppBar({
     super.key,
     required this.title,
+    this.buttons,
     this.preferredSize = const Size.fromHeight(70),
   });
 
@@ -30,7 +32,26 @@ class TopAppBar extends StatelessWidget implements PreferredSizeWidget {
         leadingWidth: 70,
         elevation: 1.0,
         shadowColor: Colors.black,
+        actions: buttons,
       ),
+    );
+  }
+}
+
+class TopAppBarWithButton extends StatelessWidget
+    implements PreferredSizeWidget {
+  final String title;
+  final List<Widget>? buttons;
+  @override
+  final Size preferredSize = const Size.fromHeight(70);
+
+  const TopAppBarWithButton({super.key, required this.title, this.buttons});
+
+  @override
+  Widget build(BuildContext context) {
+    return TopAppBar(
+      title: title,
+      buttons: buttons,
     );
   }
 }
