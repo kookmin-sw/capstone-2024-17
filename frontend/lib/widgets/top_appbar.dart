40bc922ca9d023985298254d89730077e539505a
godeka <godeka@naver.com>
2024-05-01 09:58:18 +0900

    [feat] 기본 상단 앱바 위젯 추가 #137

---
+++ frontend/lib/widgets/top_appbar.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class TopAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  @override
+  final Size preferredSize;
+
+  const TopAppBar({
+    super.key,
+    required this.title,
+    this.preferredSize = const Size.fromHeight(70),
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return PreferredSize(
+      preferredSize: preferredSize,
+      child: AppBar(
+        title: Text(
+          title,
+          style: const TextStyle(fontSize: 24),
+        ),
+        toolbarHeight: 100,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios),
+          onPressed: () => Navigator.pop(context),
+        ),
+        leadingWidth: 70,
+      ),
+    );
+  }
+}
