8d9145243a159f24e3ee829d546ed45e54d32bf0
haram8009 <haram8009@gmail.com>
2024-05-02 13:54:51 +0900

    [fix] company 로고파일 ManyToOne 으로 수정 #98

--- backend/src/main/java/com/coffee/backend/domain/company/entity/Company.java
+++ backend/src/main/java/com/coffee/backend/domain/company/entity/Company.java
@@ -1,12 +1,13 @@
 package com.coffee.backend.domain.company.entity;
-import com.coffee.backend.domain.storage.entity.UploadFile;
 
+import com.coffee.backend.domain.storage.entity.UploadFile;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -25,6 +26,7 @@ public class Company {
     private String name;
     private String domain;
     private String bno;
-    @OneToOne
+    @ManyToOne
+    @JoinColumn(name = "logo_file_id")
     private UploadFile logo;
 }
