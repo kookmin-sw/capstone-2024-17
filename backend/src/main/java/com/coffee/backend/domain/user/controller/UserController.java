e1c008c3b4fa0328754e77aaae4840e7ddd24a5c
haram8009 <haram8009@gmail.com>
2024-05-05 18:02:15 +0900

    [feat] 직무 리스트 api #169

--- backend/src/main/java/com/coffee/backend/domain/user/controller/UserController.java
+++ backend/src/main/java/com/coffee/backend/domain/user/controller/UserController.java
@@ -4,12 +4,16 @@ import com.coffee.backend.domain.auth.controller.AuthenticationPrincipal;
 import com.coffee.backend.domain.user.dto.IntroductionUpdateRequest;
 import com.coffee.backend.domain.user.dto.PositionUpdateRequest;
 import com.coffee.backend.domain.user.dto.UserDto;
+import com.coffee.backend.domain.user.entity.Position;
 import com.coffee.backend.domain.user.entity.User;
 import com.coffee.backend.domain.user.service.UserService;
 import com.coffee.backend.utils.ApiResponse;
+import java.util.Arrays;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,16 +29,18 @@ public class UserController {
     @PostMapping("/position/update")
     public ResponseEntity<ApiResponse<UserDto>> position(@AuthenticationPrincipal User user,
                                                          @RequestBody PositionUpdateRequest dto) {
-
         return ResponseEntity.ok(ApiResponse.success(userService.updateUserPosition(user, dto.getPosition())));
     }
 
     @PostMapping("/introduction/update")
     public ResponseEntity<ApiResponse<UserDto>> position(@AuthenticationPrincipal User user,
                                                          @RequestBody IntroductionUpdateRequest dto) {
-
         return ResponseEntity.ok(ApiResponse.success(userService.updateUserIntroduction(user, dto.getIntroduction())));
     }
 
-
+    @GetMapping("/position/list")
+    public ResponseEntity<ApiResponse<List<String>>> positionList() {
+        return ResponseEntity.ok(
+                ApiResponse.success(Arrays.stream(Position.values()).map(position -> position.getName()).toList()));
+    }
 }
