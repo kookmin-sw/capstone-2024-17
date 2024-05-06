6ce9722c97519777cf8a2b13a9a2e4855ce03a02
haram8009 <haram8009@gmail.com>
2024-05-07 01:31:12 +0900

    [fix] 실제 회사 요청 데이터 넘겨주기, 유저 정보 넘기기 #99

--- backend/src/main/java/com/coffee/backend/domain/company/service/CompanyRequestService.java
+++ backend/src/main/java/com/coffee/backend/domain/company/service/CompanyRequestService.java
@@ -1,12 +1,15 @@
 package com.coffee.backend.domain.company.service;
 
+import com.coffee.backend.domain.company.dto.CompanyRequestDto;
 import com.coffee.backend.domain.company.dto.CompanyRequestRequest;
 import com.coffee.backend.domain.company.entity.CompanyRequest;
 import com.coffee.backend.domain.company.repository.CompanyRequestRepository;
 import com.coffee.backend.domain.user.entity.User;
 import com.coffee.backend.exception.CustomException;
 import com.coffee.backend.exception.ErrorCode;
+import com.coffee.backend.utils.CustomMapper;
 import jakarta.transaction.Transactional;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -18,6 +21,7 @@ import org.springframework.stereotype.Service;
 public class CompanyRequestService {
 
     private final CompanyRequestRepository companyRequestRepository;
+    private final CustomMapper customMapper;
 
     public CompanyRequestRequest saveRequest(User user, CompanyRequestRequest dto) {
         companyRequestRepository.save(CompanyRequest.builder()
@@ -35,5 +39,14 @@ public class CompanyRequestService {
                     return new CustomException(ErrorCode.COMPANY_REQUEST_NOT_FOUND);
                 }
         ));
+    }
+
+    public List<CompanyRequestDto> findAllRequests() {
+        return companyRequestRepository.findAll().stream()
+                .map(cr -> CompanyRequestDto.builder().id(cr.getCompanyRequestId())
+                        .name(cr.getName())
+                        .domain(cr.getDomain())
+                        .bno(cr.getBno())
+                        .user(customMapper.toUserDto(cr.getUser())).build()).toList();
     }
 }
--- backend/src/main/java/com/coffee/backend/domain/dashboard/controller/DashboardMVCController.java
+++ backend/src/main/java/com/coffee/backend/domain/dashboard/controller/DashboardMVCController.java
@@ -1,25 +1,25 @@
 package com.coffee.backend.domain.dashboard.controller;
 
 import com.coffee.backend.domain.company.dto.CompanyRequestDto;
-import java.util.ArrayList;
+import com.coffee.backend.domain.company.service.CompanyRequestService;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
+@RequiredArgsConstructor
 @Controller
 public class DashboardMVCController {
+    private final CompanyRequestService companyRequestService;
+    private final ModelMapper mapper;
+
     @GetMapping("/dashboard")
     public String dashboard(Model model) {
-        List<CompanyRequestDto> requests = new ArrayList<>();
-        requests.add(CompanyRequestDto.builder()
-                .id(1L)
-                .user("유저이름")
-                .name("회사이름")
-                .bno("000000088")
-                .domain("google.com").build());
+        List<CompanyRequestDto> requests = companyRequestService.findAllRequests();
         model.addAttribute("requests", requests);
         return "dashboard/index";
     }
