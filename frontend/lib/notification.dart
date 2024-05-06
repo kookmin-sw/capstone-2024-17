8c203eb1cb3669efb7abfa9d931cff89be71fccf
jm3789 <jm37890@gmail.com>
2024-05-06 17:52:11 +0900

    [feat] 알림의 sentTime과 senderId도 받아오기, 코드 주석 달아서 정리 #162

--- frontend/lib/notification.dart
+++ frontend/lib/notification.dart
@@ -4,20 +4,15 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 
 // 백그라운드에서 수신된 메시지를 처리하기 위한 콜백 함수
 Future<void> onBackgroundMessage(RemoteMessage message) async {
-  await Firebase.initializeApp(); // firebase 초기화
-
-  // 알림을 notification으로 처리(어디에 쓰는거지)
-  if (message.data.containsKey('notification')) {
-    final notification = message.data['notification'];
-  }
+  await Firebase.initializeApp(); // 백그라운드일 때 firebase 초기화
 }
 
 // FCM 관련 기능을 관리하는 클래스
 class FCM {
   // StreamController 생성: 어느 타이밍에 데이터가 들어올 지 모를 때 비동기로 작업을 처리
-  final dataStreamController = StreamController<String>.broadcast();
-  final titleStreamController = StreamController<String>.broadcast();
-  final bodyStreamController = StreamController<String>.broadcast();
+  // 메시지 내용은 {'title': String?, 'body': String?, 'senttime': DateTime?, 'senderId': String?} 형태
+  final messageStreamController =
+      StreamController<Map<String, dynamic>>.broadcast();
 
   // 알림 설정
   setNotifications() {
@@ -35,12 +30,14 @@ class FCM {
     FirebaseMessaging.onMessage.listen(
       (message) async {
         // (메시지에는 notification 메시지, data 메시지가 있다)
-        if (message.data.containsKey('notification')) {
-          print('[notification forground]${message.data['notification']}');
-          dataStreamController.sink.add(message.data['notification']);
+        if (message.notification != null) {
+          messageStreamController.sink.add({
+            'title': message.notification!.title,
+            'body': message.notification!.body,
+            'senttime': message.sentTime,
+            'senderId': message.senderId,
+          });
         }
-        titleStreamController.sink.add(message.notification!.title!);
-        bodyStreamController.sink.add(message.notification!.body!);
       },
     );
   }
@@ -49,12 +46,14 @@ class FCM {
   backgroundNotification() {
     FirebaseMessaging.onMessageOpenedApp.listen(
       (message) async {
-        if (message.data.containsKey('notification')) {
-          print('[notification background]${message.data['notification']}');
-          dataStreamController.sink.add(message.data['notification']);
+        if (message.notification != null) {
+          messageStreamController.sink.add({
+            'title': message.notification!.title,
+            'body': message.notification!.body,
+            'senttime': message.sentTime,
+            'senderId': message.senderId,
+          });
         }
-        titleStreamController.sink.add(message.notification!.title!);
-        bodyStreamController.sink.add(message.notification!.body!);
       },
     );
   }
@@ -66,18 +65,18 @@ class FCM {
         await FirebaseMessaging.instance.getInitialMessage();
 
     if (initialMessage != null) {
-      if (initialMessage.data.containsKey('notification')) {
-        print('[notification terminate]${initialMessage.data['notification']}');
-        dataStreamController.sink.add(initialMessage.data['notification']);
+      if (initialMessage.notification != null) {
+        messageStreamController.sink.add({
+          'title': initialMessage.notification!.title,
+          'body': initialMessage.notification!.body,
+          'senttime': initialMessage.sentTime,
+          'senderId': initialMessage.senderId,
+        });
       }
-      titleStreamController.sink.add(initialMessage.notification!.title!);
-      bodyStreamController.sink.add(initialMessage.notification!.body!);
     }
   }
 
   dispose() {
-    dataStreamController.close();
-    bodyStreamController.close();
-    titleStreamController.close();
+    messageStreamController.close();
   }
 }
