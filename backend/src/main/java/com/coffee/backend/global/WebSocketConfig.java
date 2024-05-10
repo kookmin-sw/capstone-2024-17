a6466091cc7cfa7abddcecdab64cc3202c9858e4
hyeesw <hyeeun7904@kookmin.ac.kr>
2024-05-10 13:31:58 +0900

    [feat] 웹소켓 disconnect 감지 eventListener #198

--- backend/src/main/java/com/coffee/backend/global/WebSocketConfig.java
+++ backend/src/main/java/com/coffee/backend/global/WebSocketConfig.java
@@ -1,16 +1,31 @@
 package com.coffee.backend.global;
 
+import com.coffee.backend.domain.user.service.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 @Configuration
 @EnableWebSocket
 @EnableWebSocketMessageBroker
+@AllArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    private final UserService userService;
+    private final UserHandshakeInterceptor userHandshakeInterceptor;
+
+//    @Autowired
+//    public WebSocketConfig(UserService userService, UserHandshakeInterceptor userHandshakeInterceptor) {
+//        this.userService = userService;
+//        this.userHandshakeInterceptor = userHandshakeInterceptor;
+//    }
+
     @Override
     public void configureMessageBroker(final MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/sub"); // 구독요청
@@ -21,6 +36,15 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(final StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*")
+                .addInterceptors(userHandshakeInterceptor) // userId 추출을 위해 인터셉터 추가
                 .withSockJS();
+    }
+
+    // 웹소켓 해제 감지 -> 세션 ID, cafeId 초기화
+    @EventListener
+    public void onSessionDisconnect(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String sessionId = headerAccessor.getSessionId();
+        userService.clearSessionAndCafeIdBySessionId(sessionId);
     }
 }
