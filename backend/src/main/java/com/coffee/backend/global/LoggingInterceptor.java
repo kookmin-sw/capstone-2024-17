e89268898f6f2f635cdb95e434080b7084698cfd
hyeesw <hyeeun7904@kookmin.ac.kr>
2024-05-12 13:49:38 +0900

    [feat] request endpoint 로그 셋업 #216

---
+++ backend/src/main/java/com/coffee/backend/global/LoggingInterceptor.java
@@ -0,0 +1,20 @@
+package com.coffee.backend.global;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class LoggingInterceptor implements HandlerInterceptor {
+    private static final Logger logger = LoggerFactory.getLogger(LoggingInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        // 요청 Endpoint 로깅
+        logger.trace("!!! URL : {}", request.getRequestURI());
+        return true;
+    }
+}
