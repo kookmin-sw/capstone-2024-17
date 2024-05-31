733ed69fd40d8fecd4ad4180e893b5e9da58df6b
godeka <godeka@naver.com>
2024-05-31 10:02:33 +0900

    [fix] 평가 페이지 스크롤뷰로 #403

--- frontend/lib/screen/coffeechat_rating_screen.dart
+++ frontend/lib/screen/coffeechat_rating_screen.dart
@@ -40,122 +40,122 @@ class _CoffeeChatRatingState extends State<CoffeeChatRating> {
 
     return Scaffold(
       backgroundColor: const Color(0xFFF09676), // 배경색 설정
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 150, bottom: 0),
-              child: Text(
-                '${widget.partnerNickname}님과의 커피챗\n얼마나 만족하셨나요?',
-                style: const TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
-                ),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 100), // 이미지와 텍스트 간격 조절
-              child: SizedBox(
-                width: 300, // Stack 너비
-                height: 80, // Stack 높이
-                child: Stack(
-                  alignment: Alignment.center,
-                  children: List.generate(5, (index) {
-                    return Positioned(
-                      left: index * 60.0, // 이미지 간격 조절
-                      child: GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            selectedIndex = index;
-                          });
-                          print('평점 = ${index + 1}점');
-                          //평점 나타냄
-                        },
-                        child: Image.asset(
-                          index <= selectedIndex
-                              ? 'assets/bean_filled.png' //커피콩 채워짐
-                              : 'assets/bean_empty.png', //커피콩 비워짐
-                          width: 60,
-                          height: 60,
-                        ),
-                      ),
-                    );
-                  }),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 30),
-              child: Text(
-                selectedIndex >= 0 ? comments[selectedIndex] : '',
-                style: const TextStyle(
-                  fontSize: 20,
-                ),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 180),
-              child: GestureDetector(
-                onTap: selectedIndex >= 0
-                    ? () async {
-                        int rating = (selectedIndex + 1); //점수
-
-                        Map<String, dynamic> response = await coffeeBeanReview(
-                            widget.matchId,
-                            widget.userId,
-                            widget.partnerId,
-                            rating);
-
-                        print(response);
-
-                        await checkReviewedRequest(
-                            widget.matchId, widget.userId);
-
-                        // 커피챗 진행중인 경우에만 종료 요청
-                        if (matchingInfo.isMatching) {
-                          await matchFinishRequest(
-                            widget.matchId,
-                            widget.userId,
-                          );
-                        }
-
-                        showDialog(
-                          context: context,
-                          builder: (BuildContext context) => OneButtonDialog(
-                            content:
-                                "${widget.partnerNickname}님에게 ${selectedIndex + 1}점 반영되었습니다.\n커피챗이 종료됩니다.",
-                            onFirstButtonClick: () {
-                              Navigator.pop(context);
-                              Navigator.pop(context);
-                              selectedIndexProvider.selectedIndex = 0;
-                            },
-                          ),
-                        );
-
-                        //이동할 곳 여기 추가하면 됨!!!
-
-                        // Navigator.pop(context); // 현재 화면 닫기
-
-                        // 일단 주석 처리
-                        // Navigator.push(
-                        //     context,
-                        //     MaterialPageRoute(
-                        //         builder: (context) => const Google_Map()));
-                      }
-                    : null, // selectedIndex가 0 이상인 경우에만 클릭 가능하도록 설정
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 150, bottom: 0),
                 child: Text(
-                  selectedIndex >= 0 ? '제출하기 →' : '커피콩점을 매겨주세요.',
+                  '${widget.partnerNickname}님과의 커피챗\n얼마나 만족하셨나요?',
                   style: const TextStyle(
-                    fontSize: 22,
+                    fontSize: 25,
                     fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 100), // 이미지와 텍스트 간격 조절
+                child: SizedBox(
+                  width: 300, // Stack 너비
+                  height: 80, // Stack 높이
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: List.generate(5, (index) {
+                      return Positioned(
+                        left: index * 60.0, // 이미지 간격 조절
+                        child: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              selectedIndex = index;
+                            });
+                            print('평점 = ${index + 1}점');
+                            //평점 나타냄
+                          },
+                          child: Image.asset(
+                            index <= selectedIndex
+                                ? 'assets/bean_filled.png' //커피콩 채워짐
+                                : 'assets/bean_empty.png', //커피콩 비워짐
+                            width: 60,
+                            height: 60,
+                          ),
+                        ),
+                      );
+                    }),
                   ),
                 ),
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.only(top: 30),
+                child: Text(
+                  selectedIndex >= 0 ? comments[selectedIndex] : '',
+                  style: const TextStyle(
+                    fontSize: 20,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 180),
+                child: GestureDetector(
+                  onTap: selectedIndex >= 0
+                      ? () async {
+                          int rating = (selectedIndex + 1); //점수
+
+                          Map<String, dynamic> response =
+                              await coffeeBeanReview(widget.matchId,
+                                  widget.userId, widget.partnerId, rating);
+
+                          print(response);
+
+                          await checkReviewedRequest(
+                              widget.matchId, widget.userId);
+
+                          // 커피챗 진행중인 경우에만 종료 요청
+                          if (matchingInfo.isMatching) {
+                            await matchFinishRequest(
+                              widget.matchId,
+                              widget.userId,
+                            );
+                          }
+
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) => OneButtonDialog(
+                              content:
+                                  "${widget.partnerNickname}님에게 ${selectedIndex + 1}점 반영되었습니다.\n커피챗이 종료됩니다.",
+                              onFirstButtonClick: () {
+                                Navigator.pop(context);
+                                Navigator.pop(context);
+                                selectedIndexProvider.selectedIndex = 0;
+                              },
+                            ),
+                          );
+
+                          //이동할 곳 여기 추가하면 됨!!!
+
+                          // Navigator.pop(context); // 현재 화면 닫기
+
+                          // 일단 주석 처리
+                          // Navigator.push(
+                          //     context,
+                          //     MaterialPageRoute(
+                          //         builder: (context) => const Google_Map()));
+                        }
+                      : null, // selectedIndex가 0 이상인 경우에만 클릭 가능하도록 설정
+                  child: Text(
+                    selectedIndex >= 0 ? '제출하기 →' : '커피콩점을 매겨주세요.',
+                    style: const TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
